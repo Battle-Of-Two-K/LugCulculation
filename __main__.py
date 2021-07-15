32a270bdda44423eeaefbeb66a47117063d676c3
Battle-Of-Two-K <tr.penrose@mail.ru>
2021-07-16 00:41:39 +0300

    Continue sort out architecture this app. New variant.
    Have questions...about tk.Label and tk.Entry, do i need to transfer
    their in class "ColumnData"?

--- __main__.py
+++ __main__.py
@@ -1,11 +1,13 @@
 import tkinter as tk
+from tkinter import ttk
 from win32api import GetSystemMetrics
 
 width_screen_this_computer = GetSystemMetrics(0)
 height_screen_this_computer = GetSystemMetrics(1)
+font = "Times 14"
 
 
-class Slide(tk.Frame):
+class Slide(ttk.Frame):
     """
     Слайд с кнопкой(ами) в необходимой конфигурации.
     """
@@ -14,12 +16,12 @@ class Slide(tk.Frame):
         super().__init__(master)
 
         if slide_variant == 1:
-            self.btn_back, self.btn_next = None, tk.Button(master, text="Далее >")
+            self.btn_back, self.btn_next = None, ttk.Button(master, text="Далее >")
         elif slide_variant == 2:
-            self.btn_back, self.btn_next = tk.Button(master, text="< Назад"), \
-                                           tk.Button(master, text="Далее >")
+            self.btn_back, self.btn_next = ttk.Button(master, text="< Назад"), \
+                                           ttk.Button(master, text="Далее >")
         elif slide_variant == 3:
-            self.btn_back, self.btn_next = tk.Button(master, text="< Назад"), None
+            self.btn_back, self.btn_next = ttk.Button(master, text="< Назад"), None
         else:
             self.btn_back, self.btn_next = None, None
 
@@ -29,13 +31,9 @@ class InputInitialDataSlide(Slide):
     Слайд, на котором вводятся входные данные.
     """
 
-    frames = []
-
-    def __init__(self, master, amount_data_lines, slide_variant=1):
+    def __init__(self, master, slide_variant=1):
         super().__init__(master, slide_variant)
-
-        for i in range(amount_data_lines):
-            self.frames.append(tk.Frame(master))
+        self.frame = tk.Frame(master)
 
 
 class OutputResultSlide(Slide):
@@ -45,6 +43,7 @@ class OutputResultSlide(Slide):
 
     def __init__(self, master, slide_variant=2):
         super().__init__(master, slide_variant)
+        self.frame = tk.Frame(master)
 
 
 class EndSlide(Slide):
@@ -54,15 +53,35 @@ class EndSlide(Slide):
 
     def __init__(self, master, slide_variant=3):
         super().__init__(master, slide_variant)
+        self.frame = tk.Frame(master)
+
+
+class ColumnData(ttk.Frame):
+    set_vidgets = None
+
+    def __init__(self, master, data, index):
+        super().__init__(master)
+        self.frame = tk.Frame(master)
+
+        if index == "Label":
+            self.set_vidgets = [ttk.Label(self.frame, text=f"{row}", font=font) for row in data]
+        elif index == "Entry":
+            self.set_vidgets = [ttk.Entry(self.frame, font=font) for _ in range(len(data))]
+
+
+class ColumnDataLabel(ColumnData):
+    def __init__(self, master, data, index="Label"):
+        super().__init__(master, data, index)
+
+
+class ColumnDataEntry(ColumnData):
+    def __init__(self, master, data, index="Entry"):
+        super().__init__(master, data, index)
 
 
 class App:
     width = 720
-    height = 400
-
-    initial_slide_data = [[" Сила, вызывающая разрыв проушины - P: ", " [тс] "],
-                          [" Число поверхностей среза болта - m: ", " [шт.] "],
-                          [" Предел прочности материала болта - σ", " [(даН)/(мм^2)] "]]
+    height = 150
 
     def __init__(self):
         self.root = tk.Tk()
@@ -70,31 +89,46 @@ class App:
                            f"{int((width_screen_this_computer - self.width) / 2)}+"
                            f"{int((height_screen_this_computer - self.height) / 2)}")
 
-        self.main_frame = tk.Frame(self.root)
+        self.root.resizable(width=False, height=False)
+
+        self.main_frame = ttk.Frame(self.root)
         self.main_frame.pack()
 
         self.create_initial_slide()
 
     def create_initial_slide(self):
-        all_lines = []  # необходим, чтобы "отловить" объекты для последующих махинаций с ними
+        input_initial_data_slide = InputInitialDataSlide(self.main_frame)
 
-        input_initial_data_slide = InputInitialDataSlide(self.main_frame, len(self.initial_slide_data))
+        data = [[" Сила, вызывающая разрыв проушины - P: ", " Число поверхностей среза болта - m: ",
+                 " Предел прочности материала болта - σ:"], [None, None, None],
+                [" [тс] ", " [шт.] ", " [(даН)/(мм^2)] "]]
 
-        def create_line(text: list, master):
-            return tk.Label(master, text=f"{text[0]}"), tk.Entry(master), tk.Label(master, text=f"{text[1]}"),\
-                   tk.Checkbutton(master)
+        for info in data:
+            self.create_data_column(input_initial_data_slide.frame, info)
 
-        def pack_line(line: tuple):
-            for widget in line:
-                widget.pack(side=tk.LEFT)
-                widget.master.pack()
+        input_initial_data_slide.frame.pack()
+        input_initial_data_slide.btn_next.pack(side=tk.RIGHT, padx=30, pady=10)
 
-        if len(self.initial_slide_data) == len(input_initial_data_slide.frames):
-            for i in range(0, len(self.initial_slide_data)):
-                pack_line(create_line(self.initial_slide_data[i], input_initial_data_slide.frames[i]))
-                all_lines.append(create_line(self.initial_slide_data[i], input_initial_data_slide.frames[i]))
+    @staticmethod
+    def create_data_column(master, data):
+        if isinstance(data[0], str):
+            column = ColumnDataLabel(master, data)
 
-        input_initial_data_slide.btn_next.pack(side=tk.RIGHT)
+            for obj in column.set_vidgets:
+                obj.pack(padx=10, pady=3, anchor=tk.W)
+
+            column.frame.pack(side=tk.LEFT)
+
+        elif isinstance(data[0], type(None)):
+            column = ColumnDataEntry(master, data)
+
+            for obj in column.set_vidgets:
+                obj.pack(padx=10, pady=3, anchor=tk.W)
+
+            column.frame.pack(side=tk.LEFT)
+
+        else:
+            pass
 
     def run(self):
         self.root.mainloop()
